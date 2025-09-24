5906bd72a04a705ca0800081af7c775ae2435723
geekyanimesh <animesh.kumar9651@gmail.com>
2025-09-24 10:25:17 +0530

    Add getter/setter

--- 0_rough/rough.cpp
+++ 0_rough/rough.cpp
@@ -1,18 +1,97 @@
 #include <iostream>
 using namespace std;
 
+class Complex{
+
+private:
+    int real;
+    int img;
+
+public:
+
+    Complex(int r, int i){
+        real=r;
+        img=i;
+    }
+
+    void add(Complex &c){
+        Complex temp=c;
+        real+=temp.real;
+        img+=temp.img;
+        cout<<real<<"+"<<img<<"i"<<endl;   
+    }
+
+};
+
+class BankAccount{
+
+private:
+    string name;
+    int accountNo ;
+    float balance;
+
+public:
+
+    BankAccount(){}
+
+    // setter
+    void setName(string name){
+        this->name=name;
+    }
+
+    void setAcc(int acc){
+        accountNo=acc;
+    }
+
+    void setBalance(int b){
+        balance=b;
+    }
+
+    void deposit(int amount){
+        balance+=amount;
+    }
+
+    int withdrawl(int amount){
+        if(balance==0) return -1;
+        else{
+            balance-=amount;
+            return 0;
+        }
+    }
+
+    // getter
+
+    string getName(){
+        return name;
+    }
+
+    int getBalance(){
+        return balance;
+    }
+
+    int getAcc(){
+        return accountNo;
+    }
+
+};
+
 int main() {
     
-    int num=5;
-    int *ptr=&num;
-    (*ptr)++;
+    // Complex c1(20,13);
+    // Complex c2(45,78);
+    // c1.add(c2);
 
-    cout<<sizeof(num)<<endl;
-    cout << sizeof(ptr) << endl;
+    BankAccount axis;
+    axis.setName("Nipun");
+    axis.setAcc(54354546);
+    axis.setBalance(10000);
 
-    cout<<num;
-    
+    axis.deposit(5000);
+    axis.withdrawl(3000);
+
+    cout<<axis.getName()<<endl;
+    cout<<axis.getAcc()<<endl;
+    cout<<axis.getBalance()<<endl;
 
     return 0;
 }
-
--- 1_Pointers/arrayPointes.cpp
+++ 1_Pointers/arrayPointes.cpp
@@ -9,15 +9,30 @@ int main() {
     // cout<<arr[0]<< endl;
     // cout<<*arr<< endl;
 
-    int arr2[10]{1,2,3};
+    // int arr2[10]{1,2,3};
 
-    cout<<arr2<<endl;
-    cout<<arr2[0]<<endl;
-    cout<<*arr2<<endl;
-    cout<<*arr2+1<<endl;
-    cout << *(arr2 + 1) << endl;
-    cout << *(arr2 + 1)+1 << endl;
+    // cout<<arr2<<endl;
+    // cout<<arr2[0]<<endl;
+    // cout<<*arr2<<endl;
+    // cout<<*arr2+1<<endl;
+    // cout << *(arr2 + 1) << endl;
+    // cout << *(arr2 + 1)+1 << endl;
     
+    // Formula : arr[i] = *(arr+i)
+
+    // int arr3[]={21,44,65,788,12345};
+    
+    // cout<<arr3[1]<<endl;
+    // cout<<*(arr3+1)<<endl;
+
+    // cout<<2[arr3]<<endl; 
+
+    int arr4[]={1,2,3,4,5,6};
+
+    cout<<&arr4<<endl;
+    cout << &arr4[0] << endl;
+    cout << arr4 << endl;
+
     
 
     return 0;
