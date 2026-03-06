ca4c56f0e1036352ad2a60192d5f4977ecf123ea
Animesh Kumar <animesh.kumar9651@gmail.com>
2026-03-06 20:41:45 +0530

    hashmap

--- 0_rough/rough.cpp
+++ 0_rough/rough.cpp
@@ -1,97 +1,44 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 
-class Complex{
-
-private:
-    int real;
-    int img;
-
-public:
-
-    Complex(int r, int i){
-        real=r;
-        img=i;
-    }
-
-    void add(Complex &c){
-        Complex temp=c;
-        real+=temp.real;
-        img+=temp.img;
-        cout<<real<<"+"<<img<<"i"<<endl;   
-    }
-
-};
-
-class BankAccount{
-
-private:
-    string name;
-    int accountNo ;
-    float balance;
-
-public:
-
-    BankAccount(){}
-
-    // setter
-    void setName(string name){
-        this->name=name;
-    }
-
-    void setAcc(int acc){
-        accountNo=acc;
-    }
-
-    void setBalance(int b){
-        balance=b;
-    }
-
-    void deposit(int amount){
-        balance+=amount;
-    }
-
-    int withdrawl(int amount){
-        if(balance==0) return -1;
-        else{
-            balance-=amount;
-            return 0;
+    string decimalToBinary(int num)
+    {
+        string s;
+        while (num > 1)
+        {
+            int rem = num % 2;
+            num /= 2;
+            char x = rem + '0';
+            s.push_back(x);
         }
+        s.push_back('1');
+        reverse(s.begin(), s.end());
+        return s;
     }
 
-    // getter
-
-    string getName(){
-        return name;
+    int concatenatedBinary(int n)
+    {
+        string res;
+        for (int i = 1; i <= n; i++)
+        {
+            res += decimalToBinary(i);
+        }
+        long long result = 0;
+        int power = 0;
+        int size = res.length();
+        for (int i = size - 1; i >= 0; i--)
+        {
+            int x = res[i] - '0';
+            result += x * pow(2, power);
+            power++;
+        }
+        return result;
     }
 
-    int getBalance(){
-        return balance;
+    int main(){
+        cout << concatenatedBinary(3);
+        return 0;
     }
-
-    int getAcc(){
-        return accountNo;
-    }
-
-};
-
-int main() {
-    
-    // Complex c1(20,13);
-    // Complex c2(45,78);
-    // c1.add(c2);
-
-    BankAccount axis;
-    axis.setName("Nipun");
-    axis.setAcc(54354546);
-    axis.setBalance(10000);
-
-    axis.deposit(5000);
-    axis.withdrawl(3000);
-
-    cout<<axis.getName()<<endl;
-    cout<<axis.getAcc()<<endl;
-    cout<<axis.getBalance()<<endl;
-
-    return 0;
-}
---
+++ 29_Maps&Sets/maps/maps_2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // ordered sets & maps take complexity of - O(N)
+
+    
+
+    return 0;
+}
